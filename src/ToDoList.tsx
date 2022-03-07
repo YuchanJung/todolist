10562a9a3213eee1469b7e072cdc1147c3cde852
yuchannn <yuchanbpp@gmail.com>
2022-03-07 20:33:30 +0900

    react hook form prac

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
+  const { register, watch, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
+  /*
     const [toDo, setToDo] = useState("");
     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
         const {
@@ -12,18 +19,21 @@ function ToDoList() {
         event.preventDefault();
         console.log(toDo);
     }
-    return (
-      <div>
-        <form onSubmit={onSubmit}>
-          <input
-            onChange={onChange}
-            value={toDo}
-            placeholder="Write a to do"
-          ></input>
-          <button>Add</button>
-        </form>
-      </div>
-    );
+    */
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("todo", {
+            required: "Password is required",
+            minLength: { value: 5, message: "Password is too short" },
+          })}
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
