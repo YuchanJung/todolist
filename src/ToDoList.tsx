06afb62203cbf359ca7bb24214575e842cfd2571
yuchannn <yuchanbpp@gmail.com>
2022-03-08 15:51:27 +0900

    react hook form - formState errors prac

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  todo: string;
+  password: string;
+  pwConfirmation: string;
+  email: string;
+}
+
 function ToDoList() {
-  const { register, watch, handleSubmit, formState } = useForm();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+      defaultValues: {
+          email: "~@naver.com"
+      }
+  });
   const onValid = (data: any) => {
     console.log(data);
   };
-  console.log(formState.errors);
+  console.log(errors);
   /*
     const [toDo, setToDo] = useState("");
     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
@@ -14,7 +30,7 @@ function ToDoList() {
           currentTarget: { value },
         } = event;
         setToDo(value);
-    };
+    };b 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         console.log(toDo);
@@ -22,14 +38,48 @@ function ToDoList() {
     */
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
         <input
           {...register("todo", {
-            required: "Password is required",
-            minLength: { value: 5, message: "Password is too short" },
+            required: "Todo is required",
+            minLength: { value: 5, message: "Todo is too short" },
           })}
           placeholder="Write a to do"
         />
+        <span>{errors?.todo?.message}</span>
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: { value: 8, message: "Password is too short" },
+          })}
+          placeholder="Password"
+        />
+        <span>{errors.password?.message}</span>
+        <input
+          {...register("pwConfirmation", {
+            required: "Password Confirmation is required",
+            minLength: {
+              value: 8,
+              message: "Password Confirmation is too short",
+            },
+          })}
+          placeholder="password confirmation"
+        />
+        <span>{errors.pwConfirmation?.message}</span>
         <button>Add</button>
       </form>
     </div>
