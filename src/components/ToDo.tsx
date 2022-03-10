880c97e524704dba0509ac3658f7b8258d6eff9a
yuchannn <yuchanbpp@gmail.com>
2022-03-10 20:06:16 +0900

    make category btn

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,7 +1,30 @@
+import React from "react";
 import { IToDo } from "../atom";
 
-function ToDo({ text }: IToDo) {
-  return <li>{text}</li>;
+function ToDo({ text, category, id }: IToDo) {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.name);
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
 }
 
 export default ToDo;
