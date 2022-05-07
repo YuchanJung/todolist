2e3e1a6af3f198a2949c6ab966aa2543e661c291
yuchannn <yuchanbpp@gmail.com>
2022-05-07 11:02:04 +0900

    ellipsis icon

---
+++ src/components/icons/EllipsisVerticalIcon.tsx
@@ -0,0 +1,13 @@
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function EllipsisVerticalIcon() {
+  return (
+    <FontAwesomeIcon
+      icon={faEllipsisVertical}
+      className="ellipsisVertical"
+    ></FontAwesomeIcon>
+  );
+}
+
+export default EllipsisVerticalIcon;
